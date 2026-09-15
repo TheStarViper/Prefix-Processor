acfaf792f27b4173e4f4e70676c8ccd55c2bc4f2
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-14 21:26:41 -0400

    move cpp test to cpp_test

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,19 +1,5 @@
 <script lang="ts">
-	import MainModuleFactory from "$lib/cpp/cpp_module";
-	import { onMount } from "svelte";
-
-	let addRes: number = $state(0);
-	let multRes: number = $state(0);
-
-	onMount(async () => {
-		const Module = await MainModuleFactory();
-
-		addRes = Module._add_numbers(3, 4);
-		multRes = Module._multiply_floats(3, 4);
-	});
+	import Game from "$lib/components/Game.svelte";
 </script>
 
-<h1>test of importing a function from c++</h1>
-
-<p>add_numbers test: 3 + 4 = {addRes}</p>
-<p>multiply_floats test: 3 * 4 = {multRes}</p>
+<Game />
