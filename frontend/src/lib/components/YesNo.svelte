0c43a04e4dcc8dd5ad55e517d2b2ecc5da4391d0
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-15 00:04:20 -0400

    stylize buttons

--- frontend/src/lib/components/YesNo.svelte
+++ frontend/src/lib/components/YesNo.svelte
@@ -53,5 +53,31 @@
 	button {
 		height: 100%;
 		width: 100%;
+		border-radius: 0.6em;
+		background: var(--button-bg);
+		border: 2px solid color-mix(in srgb, var(--button-bg) 90%, black);
+
+		font-size: 2rem;
+		color: #111;
+
+		box-shadow: 0 4px 15px rgba(0, 0, 0, 0.4);
+
+		transition:
+			filter 0.1s var(--timing),
+			transform 0.2s var(--timing),
+			box-shadow 0.2s var(--timing);
+	}
+
+	button:hover {
+		filter: saturate(130%);
+		transform: translateY(-0.15em);
+		box-shadow: 0 4px 15px rgba(0, 0, 0, 0.6);
+	}
+
+	#yes {
+		--button-bg: #98c379;
+	}
+	#no {
+		--button-bg: #ef657a;
 	}
 </style>
