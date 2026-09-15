8581d1e7b56b9f4a97322933af5297cebcb1df7f
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-14 20:20:06 -0400

    add wasm test in sveltekit

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import MainModuleFactory from "$lib/cpp/cpp_module";
+	import { onMount } from "svelte";
+
+	let addRes: number = $state(0);
+	let multRes: number = $state(0);
+
+	onMount(async () => {
+		const Module = await MainModuleFactory();
+
+		addRes = Module._add_numbers(3, 4);
+		multRes = Module._multiply_floats(3, 4);
+	});
+</script>
+
+<h1>test of importing a function from c++</h1>
+
+<p>add_numbers test: 3 + 4 = {addRes}</p>
+<p>multiply_floats test: 3 * 4 = {multRes}</p>
