5528f7a1b16d7b36858c76cb8a762261fac789e5
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-15 21:31:17 -0400

    make frontend use backend

--- frontend/src/lib/logic/index.ts
+++
@@ -1,6 +0,0 @@
-import * as placeholders from "./placeholders";
-
-export const getWord: () => string = placeholders.placeholderGetWord;
-export const getPrefix: () => string = placeholders.placeholderGetPrefix;
-export const isValid: (word: string, prefix: string) => boolean =
-	placeholders.placeholderIsValid;
